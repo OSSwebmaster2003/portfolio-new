aa0f445f4337b596692fab3dd8dfc5e765900902
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2024-03-24 06:03:28 -0700

    Banner section changed successfully

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "../assets/avatar.svg";
-import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+import { FaGithub, FaTelegramPlane, FaLinkedinIn } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
+import { Link } from "react-scroll";
 
 const Banner = () => {
   return (
@@ -13,7 +14,6 @@ const Banner = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col items-center gap-y-8 lg:flex-row lg:gap-x-12">
-          {/* text  */}
           <div className="flex-1 text-center font-secondary lg:text-left">
             <motion.h1
               variants={fadeIn("up", 0.3)}
@@ -22,7 +22,7 @@ const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
               className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
             >
-              BEN <span>AIDEN</span>
+              Otabek <span>Sodiqov</span>
             </motion.h1>
             <motion.div
               variants={fadeIn("up", 0.4)}
@@ -33,7 +33,7 @@ const Banner = () => {
             >
               <span className="mr-4 text-white">I am a</span>
               <TypeAnimation
-                sequence={["Developer", 1000, "Coder", 1000]}
+                sequence={["Developer", 1000, "Programmer", 1000]}
                 speed={150}
                 className="text-accent"
                 wrapper="span"
@@ -47,10 +47,13 @@ const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
               className="max-w-lg mx-auto mb-8 lg:mx-0"
             >
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              ipsa ab, magnam natus praesentium at tempore totam dolorem
-              incidunt nobis nihil, similique culpa exercitationem itaque eaque
-              rerum facilis alias molestias?
+              "Welcome to Otabek's World of Web Development! I'm a frontend
+              developer specializing in crafting captivating user experiences.
+              With expertise in <strong>HTML</strong>, <strong>CSS</strong>,{" "}
+              <strong>JavaScript</strong> and frameworks like{" "}
+              <strong>React</strong>, I bring designs to life with precision and
+              creativity. Let's collaborate to build stunning, responsive
+              websites that leave a lasting impression. Let's connect today!"
             </motion.p>
             <motion.div
               variants={fadeIn("up", 0.6)}
@@ -59,10 +62,18 @@ const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
               className="flex items-center mx-auto mb-12 max-w-max gap-x-6 lg:mx-0"
             >
-              <button className="btn btn-lg">Contact me</button>
-              <a href="/#" className="text-gradient btn-link">
+              <button className="btn btn-lg">
+                <Link to="contact" smooth={true}>
+                  Contact me
+                </Link>
+              </button>
+              <Link
+                to="work"
+                smooth={true}
+                className="cursor-pointer text-gradient btn-link"
+              >
                 My Portfolio
-              </a>
+              </Link>
             </motion.div>
             <motion.div
               variants={fadeIn("up", 0.8)}
@@ -71,18 +82,29 @@ const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
               className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
             >
-              <a href="/#">
-                <FaYoutube />
+              <a
+                href="http://t.me/Otabek_Sobirjonovich"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTelegramPlane />
               </a>
-              <a href="/#">
+              <a
+                href="https://github.com/OSSwebmaster2003"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaGithub />
               </a>
-              <a href="/#">
-                <FaDribbble />
+              <a
+                href="https://www.linkedin.com/in/otabek-s-081013266/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
               </a>
             </motion.div>
           </div>
-          {/* image  */}
           <motion.div
             variants={fadeIn("down", 0.5)}
             initial="hidden"
